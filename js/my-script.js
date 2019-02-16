5a7d7e799ef700ae1442dd21da5a65811800b738
SAYri <34859317+SAYri-ua@users.noreply.github.com>
2019-02-16 11:28:30 +0200

    up
    Update my-script

--- js/my-script.js
+++ js/my-script.js
@@ -1,6 +1,46 @@
 (function ($, undefined) {
   jQuery(document).ready(function ($) {
 
+    //Тестовая пидкрузка новостей
+    $('.panel #all').click(function (event) {
+      event.preventDefault()
+
+      if ($('.panel_news.uk-active .uk-grid div.clone').length != 0) {
+        var e = $('.panel_news.uk-active .uk-grid div.clone')
+
+        for (var i = 0; i < 4; i++) {
+          e.eq(i).clone().appendTo('.panel_news.uk-active .uk-grid')
+        }
+
+      }
+      else {
+        $('.panel_news.uk-active .uk-grid div.uk-hidden').
+          addClass('uk-grid-margin clone')
+        $('.panel_news.uk-active .uk-grid div.clone').removeClass('uk-hidden')
+
+      }
+    })
+
+    $('.panel #2019').click(function (event) {
+      event.preventDefault()
+
+      if ($('.panel_news.uk-active .uk-grid div.clone').length != 0) {
+        var e = $('.panel_news.uk-active .uk-grid div.clone')
+
+        for (var i = 0; i < 4; i++) {
+          e.eq(i).clone().appendTo('.panel_news.uk-active .uk-grid')
+        }
+
+      }
+      else {
+        $('.panel_news.uk-active .uk-grid div.uk-hidden').
+          addClass('uk-grid-margin clone')
+        $('.panel_news.uk-active .uk-grid div.clone').removeClass('uk-hidden')
+
+      }
+    })
+    // Тестовая пидкрузка новостей
+
     $('.mob-search-button.icon-search').on('click', function () {
       $('.mob-search-button.icon-search').addClass('uk-hidden')
       $('.menu-modal-header span').css('visibility', 'hidden')
@@ -25,7 +65,9 @@
     })
 
     $('.mob-close-button').click(function () {
-      $('.modal-menu-dropdown').find('.modal-nav-active').removeClass('.modal-nav-active')
+      $('.modal-menu-dropdown').
+        find('.modal-nav-active').
+        removeClass('.modal-nav-active')
       $('.mob-search-button.icon-search').removeClass('uk-hidden')
       $('.mob-search-button.icon-chevron-left').addClass('uk-hidden')
     })
@@ -36,70 +78,104 @@
       $('.mob-search-button.icon-chevron-left').addClass('uk-hidden')
     })
 
+    $('td').on('mouseover mouseout', function () {
+      $(this).prevAll().addBack().add($(this).parent().prevAll().children(':nth-child(' + ($(this).index() + 1) + ')')).toggleClass('hover-table')
+    })//для табличек
 
-    $('td').on('mouseover mouseout', function(){
-      $(this).prevAll().addBack()
-      .add($(this).parent().prevAll()
-      .children(':nth-child(' + ($(this).index() + 1) + ')')).toggleClass('hover-table');
-    });
-
-    $('.panel #all').click(function (event) {
-      event.preventDefault()
-      //console.log(test);
-
-      if ( $('.panel_news.uk-active .uk-grid div.clone').length != 0 ){
-        var e = $('.panel_news.uk-active .uk-grid div.clone')
-
-        for (var i = 0; i < 4; i++) {
-          e.eq(i).clone().appendTo('.panel_news.uk-active .uk-grid');
-        }
-
-      } else {
-        $('.panel_news.uk-active .uk-grid div.uk-hidden').addClass('uk-grid-margin clone')
-        $('.panel_news.uk-active .uk-grid div.clone').removeClass('uk-hidden')
-
-      }
-
-    })
-
-    $('.panel #2019').click(function (event) {
-      event.preventDefault()
-      //console.log(test);
-
-      if ( $('.panel_news.uk-active .uk-grid div.clone').length != 0 ){
-        var e = $('.panel_news.uk-active .uk-grid div.clone')
-
-        for (var i = 0; i < 4; i++) {
-          e.eq(i).clone().appendTo('.panel_news.uk-active .uk-grid');
-        }
-
-      } else {
-        $('.panel_news.uk-active .uk-grid div.uk-hidden').addClass('uk-grid-margin clone')
-        $('.panel_news.uk-active .uk-grid div.clone').removeClass('uk-hidden')
-
-      }
-
-    })
-
-    $(function() {
+    $(function () {
       var nice =
-        $(".nicescroll-box").niceScroll({
-          cursorcolor:"black",
-          cursorborder: "0",
+        $('.nicescroll-box').niceScroll({
+          cursorcolor: 'black',
+          cursorborder: '0',
           emulatetouch: true,
-        });
+        })
 
-      var _super = nice.getContentSize;
+      var _super = nice.getContentSize
 
       nice.getContentSize = function () {
-        var page = _super.call(nice);
-        page.h = nice.win.height();
-        return page;
+        var page = _super.call(nice)
+        page.h = nice.win.height()
+        return page
       }
 
-      $('.nicescroll-rails.nicescroll-rails-vr').remove();
+      $('.nicescroll-rails.nicescroll-rails-vr').remove()
 
-    });//nicescroll
+    })//nicescroll
+
+    ymaps.ready(init);
+
+    function init () {
+      var myMap = new ymaps.Map('frontpage-map', {
+          center: [56.315411, 44.027115],
+          zoom: 13,
+        }),
+        collection = new ymaps.GeoObjectCollection(),
+        bounds = myMap.getBounds()
+      var myPlacemark1 = new ymaps.Placemark([56.315411, 44.027115], {
+        hintContent: 'ул.Генкиной',
+        balloonContent: '',
+      }, {
+        iconLayout: 'default#image',
+      })
+      myMap.geoObjects.add(myPlacemark1)
+
+      var myMap = new ymaps.Map('frontpage-map2', {
+          center: [56.240568, 43.523753],
+          zoom: 13,
+        }),
+        collection = new ymaps.GeoObjectCollection(),
+        bounds = myMap.getBounds()
+      var myPlacemark2 = new ymaps.Placemark([56.240568, 43.523753], {
+        hintContent: 'ул. 1 Мая',
+        balloonContent: '',
+      }, {
+        iconLayout: 'default#image',
+      })
+      myMap.geoObjects.add(myPlacemark2)
+
+      var myMap = new ymaps.Map('frontpage-map3', {
+          center: [55.651374, 37.522108],
+          zoom: 13,
+        }),
+        collection = new ymaps.GeoObjectCollection(),
+        bounds = myMap.getBounds()
+      var myPlacemark3 = new ymaps.Placemark([55.651374, 37.522108], {
+        hintContent: 'ул. Бутлерова',
+        balloonContent: '',
+      }, {
+        iconLayout: 'default#image',
+      })
+      myMap.geoObjects.add(myPlacemark3)
+
+      var myMap = new ymaps.Map('frontpage-map4', {
+          center: [56.240568, 43.523753],
+          zoom: 10,
+        }),
+        collection = new ymaps.GeoObjectCollection(),
+        bounds = myMap.getBounds()
+      var myPlacemark4 = new ymaps.Placemark([56.240568, 43.523753], {
+        hintContent: 'ул. 1 Мая',
+        balloonContent: '',
+      }, {
+        iconLayout: 'default#image',
+      })
+      myMap.geoObjects.add(myPlacemark4)
+
+      var myMap = new ymaps.Map('frontpage-map5', {
+          center: [55.452436, 37.292194],
+          zoom: 10,
+        }),
+        collection = new ymaps.GeoObjectCollection(),
+        bounds = myMap.getBounds()
+      var myPlacemark5 = new ymaps.Placemark([55.452436, 37.292194], {
+        hintContent: 'Калужское шоссе',
+        balloonContent: '',
+      }, {
+        iconLayout: 'default#image',
+      })
+      myMap.geoObjects.add(myPlacemark5)
+
+    }
 
   })
 })(jQuery)
