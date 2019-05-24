2a432fb2a031f1334bdb78c7af1558fd1da51fac
Sergei Birukov <suxscribe@gmail.com>
2019-05-24 15:55:44 +0300

    up

---
+++ bim.php
@@ -0,0 +1,27 @@
+<? include 'header.php'; ?>
+
+	<main>
+
+
+		<div class="uk-container container-965 uk-margin-large-bottom">
+			<h1 class="block-fullwidth-heading h1-titke">BIM Моделирование</h1>
+		</div>
+
+
+		<div class="uk-container container-965 uk-margin-large-bottom">
+
+			<div class="uk-grid uk-grid-width uk-grid-small-1-1 uk-grid-width-medium-1-2 uk-grid-width-large-1-3 uk-margin-large-bottom uk-flex-center" data-uk-grid-margin data-uk-scrollspy="{cls:'uk-animation-slide-bottom',delay:250}">
+				<div>
+					<div class="certificate-item certificate-item_bim">
+						<div class="certificate-item-title certificate-item-title_bim">BIM Моделирование</div>
+						<div class="certificate-item-desc">Цифровые библиотеки и обмен BIM-контентом для проектировщиков и производителей</div>
+						<div class="certificate-item-button"><a href="https://bimlib.pro/manufacturer/aohemkor/3122" class="uk-button uk-button-large" target="_blank">Перейти на bimlib.pro</a></div>
+					</div>
+				</div>
+
+	</main>
+
+
+
+
+<? include 'footer.php'; ?>
