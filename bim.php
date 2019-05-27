9095571a11965c95c23e2ecf0c1fbb0092216e47
suxscribe <suxscribe@gmail.com>
2019-05-27 08:18:15 +0300

    up

--- bim.php
+++ bim.php
@@ -10,7 +10,7 @@
 
 		<div class="uk-container container-965 uk-margin-large-bottom">
 
-			<div class="uk-grid uk-grid-width uk-grid-small-1-1 uk-grid-width-medium-1-2 uk-grid-width-large-1-3 uk-margin-large-bottom uk-flex-center" data-uk-grid-margin data-uk-scrollspy="{cls:'uk-animation-slide-bottom',delay:250}">
+			<div class="uk-grid uk-grid-width uk-grid-small-1-1 uk-grid-width-medium-1-2 uk-grid-width-large-1-3 uk-margin-large-bottom uk-flex-center" data-uk-grid-match="{target:'div>.certificate-item'}" data-uk-grid-margin data-uk-scrollspy="{cls:'uk-animation-slide-bottom',delay:250}">
 				<div>
 					<div class="certificate-item certificate-item_bim">
 						<div class="certificate-item-title certificate-item-title_bim">BIM Моделирование</div>
@@ -18,7 +18,15 @@
 						<div class="certificate-item-button"><a href="https://bimlib.pro/manufacturer/aohemkor/3122" class="uk-button uk-button-large" target="_blank">Перейти на bimlib.pro</a></div>
 					</div>
 				</div>
-
+				<div>
+					<div class="certificate-item certificate-item_bim">
+						<div class="certificate-item-title certificate-item-title_bim">BIM Моделирование</div>
+						<div class="certificate-item-desc">Цифровые библиотеки и обмен BIM-контентом для проектировщиков и производителей</div>
+						<div class="certificate-item-button"><a href="https://bimlib.pro/manufacturer/aohemkor/3122" class="uk-button uk-button-large" target="_blank">Перейти на bimlib.pro</a></div>
+					</div>
+				</div>
+			</div>
+		</div>
 	</main>
 
 
