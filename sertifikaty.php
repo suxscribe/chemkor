6facdd2737d9e1acda988b9a8cfb3255be38c29d
suxscribe <suxscribe@gmail.com>
2019-06-20 14:47:02 +0300

    up

--- sertifikaty.php
+++ sertifikaty.php
@@ -10,7 +10,8 @@
 
 		<div class="uk-container container-965 uk-margin-large-bottom">
 
-			<div class="uk-grid uk-grid-width uk-grid-small-1-1 uk-grid-width-medium-1-2 uk-grid-width-large-1-3 uk-margin-large-bottom" data-uk-grid-margin data-uk-scrollspy="{cls:'uk-animation-slide-bottom',delay:250}">
+			<div class="uk-grid uk-grid-width uk-grid-small-1-1 uk-grid-width-medium-1-2 uk-grid-width-large-1-3 uk-margin-large-bottom" data-uk-grid-match="{target: '.certificate-item'}" data-uk-grid-margin data-uk-scrollspy="{cls:'uk-animation-slide-bottom',delay:250}">
+				
 				<div>
 					<div class="certificate-item">
 						<div class="certificate-item-title">Стандарт организации СТО 62106559-001-2017</div>
@@ -24,8 +25,8 @@
 
 				<div>
 					<div class="certificate-item">
-						<div class="certificate-item-title">Стандарт организации СТО 62106559-001-2017</div>
-						<div class="certificate-item-desc">Общие требования к компетентности испытательных и калибровочных лабораторий</div>
+						<div class="certificate-item-title">Стандарт организации </div>
+						<div class="certificate-item-desc">Общие требования к компетентности </div>
 						<div class="certificate-item-overlay">
 							<img src="images/certificate-teaser.jpg" alt="">
 						</div>
